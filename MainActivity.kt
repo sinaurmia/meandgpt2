eb675c513924cb1e2bb5cd03a66ca382c7f66030
sinaurmia <sina.urmia.92@gmail.com>
2026-08-10 01:12:35 +0330

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -424,12 +424,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnMenu.setOnClickListener {
-
             startActivity(
                 Intent(
                     this,
                     SettingsActivity::class.java
                 )
+            )
+
+            overridePendingTransition(
+                R.anim.slide_in_from_right,
+                R.anim.slide_out_to_left
             )
         }
     }
--- SettingsActivity.kt
+++ SettingsActivity.kt
@@ -46,6 +46,11 @@ class SettingsActivity : AppCompatActivity() {
         }
         btnBack.setOnClickListener {
             finish()
+
+            overridePendingTransition(
+                R.anim.slide_in_from_left,
+                R.anim.slide_out_to_right
+            )
         }
     }
 
